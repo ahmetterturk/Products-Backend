9c96246b3bc9cd2a0dfa7ddb640f92aecd254154
ahmetterturk <ahm.ert@hotmail.com>
2024-03-10 11:20:22 +1100

    get all users added

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,6 +3,16 @@ import mongoose from 'mongoose'
 
 import User from '../models/User.js';
 
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 export const createUser = async (req, res) => {
   try {
     const { email, password, isAdmin, permissions } = req.body;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { createUser, editUser, login } from '../controllers/usersController.js';
+import { getAllUsers, createUser, editUser, login } from '../controllers/usersController.js';
 
 const router = express.Router();
 
+router.get('/', getAllUsers);
 router.post('/', createUser);
 router.patch('/:id', editUser);
 router.post('/login', login);
