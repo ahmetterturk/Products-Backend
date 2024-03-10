9ba1857e1d676dcd7fba4d625e058299e178e089
ahmetterturk <ahm.ert@hotmail.com>
2024-03-10 12:31:51 +1100

    added a get single user route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -13,6 +13,22 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+export const getSingleUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 export const createUser = async (req, res) => {
   try {
     const { email, password, isAdmin, permissions } = req.body;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getAllUsers, createUser, editUser, login } from '../controllers/usersController.js';
+import { getAllUsers, getSingleUser, createUser, editUser, login } from '../controllers/usersController.js';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
+router.get('/:id', getSingleUser);
 router.post('/', createUser);
 router.patch('/:id', editUser);
 router.post('/login', login);
